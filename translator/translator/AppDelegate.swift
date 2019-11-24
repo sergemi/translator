f181f83877e73fa3bc5ba6ca138dba821c3ba6ad
Sergii Mikushev <sergemi.kh@gmail.com>
2019-11-24 21:36:11 +0200

    Initial application setup

---
+++ translator/translator/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  translator
+//
+//  Created by sergemi on 24.11.2019.
+//  Copyright © 2019 sergemi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
